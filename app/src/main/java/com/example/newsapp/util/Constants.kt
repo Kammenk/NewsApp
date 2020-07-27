48dcc650b5160724623a8f417085dd5cb9d930da
Kamen Kostov <kamenkostov@Kamens-MacBook-Air.local>
2020-07-27 17:00:57 +0300

    room

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.util
+
+class Constants {
+
+    companion object{
+        const val API_KEY = "4fb0d92753424456b8d71e311a7cc179"
+        const val BASE_URL = "http://newsapi.org/v2/"
+    }
+}
